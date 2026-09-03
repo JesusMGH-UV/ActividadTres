48ededa49be3d8476d948b890bcabe8531f4a908
Jesus Hipolito <zS24016733@estudiantes.uv.mx>
2026-09-03 13:27:06 -0600

    Actividad 3 de Eminus
    Se entregó la actividad 3 de Eminus, en ella se creó una clase TV la cual podia subir y bajar volumen, establecer, avanzar y retroceder el canal, apagar y prender la TV.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+
+    TV tv1 = new TV();
+
+    tv1.turnOn();
+    tv1.setChannel(10);
+    tv1.channelUp();
+    tv1.channelDown();
+    tv1.volumeUp();
+    tv1.volumeDown();
+    tv1.turnOff();
+}
--- src/TV.java
+++ src/TV.java
@@ -1,2 +1,68 @@
-public class TV {
+public class TV{
+
+        private int channel;
+        private int volumeLevel;
+        private boolean on;
+
+        public TV (){
+            this.channel = channel;
+            this.volumeLevel = 50;
+            this.on = on;
+        }
+
+        // Metodos
+        public void turnOn() {
+            on = true;
+            System.out.println("¡La TV se ha prendido!");
+        }
+
+        public void turnOff() {
+            on = false;
+            System.out.println("¡La TV se ha apagado!");
+        }
+
+        public void setChannel(int newChannel){
+            if (on && newChannel <= 100 && newChannel >= 1) {
+                channel = newChannel;
+                System.out.println("Se ha establecido el canal " + newChannel);
+            } else {
+                System.out.println("La TV está apagada");
+            }
+        }
+
+        public void channelUp() {
+            if (on && channel <= 100) {
+                channel++;
+                System.out.println("Se ha avanzado un canal (canal actual: " + channel + ")");
+            } else {
+                System.out.println("La TV está apagada");
+            }
+        }
+
+        public void channelDown(){
+            if (on && channel >= 1) {
+                channel--;
+                System.out.println("Se ha avanzado un canal (canal actual: " + channel + ")");
+            } else {
+                System.out.println("La TV está apagada");
+            }
+        }
+
+        public void volumeUp() {
+            if (on && volumeLevel <= 100) {
+                volumeLevel++;
+                System.out.println("Se ha subido el volumen (volumen actual: " + volumeLevel + ")");
+            } else {
+                System.out.println("La TV está apagada");
+            }
+        }
+
+        public void volumeDown() {
+            if (on && volumeLevel >= 1) {
+                volumeLevel--;
+                System.out.println("Se ha bajado el volumen (volumen actual: " + volumeLevel + ")");
+            } else {
+                System.out.println("La TV está apagada");
+            }
+        }
 }
